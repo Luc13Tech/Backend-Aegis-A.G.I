917dc687256a270dbe25afa5deff837de7390131
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-18 12:50:39 +0000

    Add files via upload

--- src/modules/auditLog/auditLog.service.js
+++ src/modules/auditLog/auditLog.service.js
@@ -1,1 +1,14 @@
+const AuditLog = require('../../models/AuditLog');
 
+async function getRecentLogs({ limit = 100, resourceType, userId } = {}) {
+  const filter = {};
+  if (resourceType) filter.resourceType = resourceType;
+  if (userId) filter.user = userId;
+
+  return AuditLog.find(filter)
+    .sort({ timestamp: -1 })
+    .limit(limit)
+    .populate('user', 'name email role');
+}
+
+module.exports = { getRecentLogs };
